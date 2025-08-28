010b04e0cf4a814ec19ac946aaab5041b07dc885
Anthony Muller <anthonyvsmuller@gmail.com>
2025-08-28 16:12:15 -0300

    feat: add register user

---
+++ back/internal/auth/application/usecase/register/dto.go
@@ -0,0 +1,12 @@
+package register
+
+type Input struct {
+	FirstName string
+	LastName  string
+	Email     string
+	Password  string
+}
+
+type Output struct {
+	ID string
+}
---
+++ back/internal/auth/application/usecase/register/register.go
@@ -0,0 +1,59 @@
+package register
+
+import (
+	"github.com/college-redberry/open-adoption/internal/auth/application/service"
+	errs "github.com/college-redberry/open-adoption/internal/auth/domain/error"
+	"github.com/college-redberry/open-adoption/internal/auth/domain/user"
+)
+
+type Register struct {
+	userRepo    user.UserRepo
+	hashService service.HashService
+}
+
+func New(userRepo user.UserRepo, hashService service.HashService) *Register {
+	return &Register{
+		userRepo:    userRepo,
+		hashService: hashService,
+	}
+}
+
+func (usecase *Register) Execute(input Input) (Output, error) {
+	err := user.Email(input.Email).Validate()
+	if err != nil {
+		return Output{}, err
+	}
+
+	err = user.Password(input.Password).Validate()
+	if err != nil {
+		return Output{}, err
+	}
+
+	existingUser, err := usecase.userRepo.GetByEmail(input.Email)
+	if err != nil {
+		return Output{}, errs.ErrInternal.Wrap("failed to retrieve user", err)
+	}
+
+	if existingUser.ID != "" {
+		return Output{}, errs.ErrInvalidData.Wrap("already used email", err)
+	}
+
+	hashedPassword, err := usecase.hashService.Hash(input.Password)
+	if err != nil {
+		return Output{}, errs.ErrInternal.Wrap("failed to validate password", err)
+	}
+
+	newUser := user.New(user.UserProps{
+		FirstName: input.FirstName,
+		LastName:  input.LastName,
+		Email:     user.Email(input.Email),
+		Password:  user.Password(hashedPassword),
+	})
+
+	err = usecase.userRepo.Create(newUser)
+	if err != nil {
+		return Output{}, errs.ErrInternal.Wrap("failed to create user", err)
+	}
+
+	return Output{ID: newUser.ID}, nil
+}
